7ffea78857483c1dccf8257c30ace27d306f351e
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2019-03-27 15:14:40 -0600

    added new route: markAsDone

--- main.go
+++ main.go
@@ -19,57 +19,10 @@ func main() {
 	router.HandleFunc("/tasks", getAllTasks).Methods("GET")
 	router.HandleFunc("/tasks", newTask).Methods("POST")
 	router.HandleFunc("/tasks", deleteTask).Methods("DELETE")
+	router.HandleFunc("/tasks", markAsDone).Methods("PUT")
 	router.HandleFunc("/tasks/{id}", getIDTask).Methods("GET")
 
 	if err := http.ListenAndServe(":9000", router); err != nil {
 		log.Fatal(err)
 	}
 }
-
-// ctx := context.Background()
-// conf := &firebase.Config{
-// 	DatabaseURL: "https://mytodo-microservice.firebaseio.com/",
-// }
-
-// opt := option.WithCredentialsFile("key.json")
-
-// app, err := firebase.NewApp(ctx, conf, opt)
-// if err != nil {
-// 	log.Fatalln("Error initializing app:", err)
-// }
-
-// client, err := app.Database(ctx)
-// if err != nil {
-// 	log.Fatalln("Error initializing database client:", err)
-// }
-
-/* Way to save 1
-task := map[string]*Task{
-	"26-03-2019--17:43:00": {
-		Desc: "A beaitful task",
-		Opcb: "Lmao",
-	},
-}
-ref := client.NewRef("/")
-taskRef := ref.Child("v1/tasks/")
-if err := taskRef.Set(ctx, task); err != nil {
-	log.Fatal("Error setting value:", err)
-}
-*/
-
-/* Way to save 2
-ref := client.NewRef("/v1/todo")
-if _, err := ref.Push(ctx, &Task{
-	Desc: "A bad task",
-	Date: time.Now().Format("2006-01-02 15:04"),
-	Done: true,
-}); err != nil {
-	log.Fatal("error while setting value", err)
-}
-*/
-// ref := client.NewRef("/v1")
-// var data map[string]interface{}
-// if err := ref.Get(ctx, &data); err != nil {
-// 	log.Fatalln("Error reading from database:", err)
-// }
-// fmt.Println(data)
