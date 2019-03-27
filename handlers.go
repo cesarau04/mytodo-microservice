16d02d94b268c8f79f09f01941c4b95598186281
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2019-03-27 15:15:39 -0600

    newTask, deleteTask, markAsDone, getIDTask => done

--- handlers.go
+++ handlers.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
@@ -19,12 +22,12 @@ var opt = option.WithCredentialsFile("key.json")
 var app, _ = firebase.NewApp(ctx, conf, opt)
 var client, _ = app.Database(ctx)
 
+// Useful paths
 var rootPathDB = "/"
 var todoPathDB = "/v1/todo/"
 var donePathDB = "/v1/done/"
 
 func getAllTasks(w http.ResponseWriter, r *http.Request) {
-	// Here goes the logic to call Firebase to retrieve the tasks from DB.
 	todoRef := client.NewRef(todoPathDB)
 	doneRef := client.NewRef(donePathDB)
 	var todoTasks map[string]Task
@@ -35,24 +38,74 @@ func getAllTasks(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Task to complete:\n")
 	for key, value := range todoTasks {
-		fmt.Fprintf(w, "ID: %s\nTitle: %s\nDesc: %s\nDate: %s\n", key, value.Title, value.Desc, value.Date)
+		fmt.Fprintf(w, "\tID: %s\n\tTitle: %s\n\tDesc: %s\n\tDate: %s\n\n", key, value.Title, value.Desc, value.Date)
 	}
 
 	if err := doneRef.Get(ctx, &doneTasks); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Fprintf(w, "Task already done\n")
+	fmt.Fprintf(w, "\nTask already done:\n")
 	for key, value := range doneTasks {
-		fmt.Fprintf(w, "ID: %s\nTitle: %s\nDesc: %s\nDate: %s\n", key, value.Title, value.Desc, value.Date)
+		fmt.Fprintf(w, "\tID: %s\n\tTitle: %s\n\tDesc: %s\n\tDate: %s\n\n", key, value.Title, value.Desc, value.Date)
 	}
 }
 
 func newTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	ref := client.NewRef(todoPathDB)
 
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		log.Fatal(err)
+	}
+
+	newRef, err := ref.Push(ctx, task)
+	if err != nil {
+		log.Fatal("error while setting value", err)
+	}
+	fmt.Fprintf(w, "Successfully added with key: %s", newRef.Key)
 }
 func deleteTask(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "delete")
+	var deleteID string
+
+	if err := json.NewDecoder(r.Body).Decode(&deleteID); err != nil {
+		log.Fatal(err)
+	}
+	ref := client.NewRef(todoPathDB + deleteID)
+	if err := ref.Delete(ctx); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "Deleted %s", deleteID)
 }
+
+func markAsDone(w http.ResponseWriter, r *http.Request) {
+	var updateID string
+	var dataToMove Task
+
+	if err := json.NewDecoder(r.Body).Decode(&updateID); err != nil {
+		log.Fatal(err)
+	}
+	ref := client.NewRef(todoPathDB + updateID)
+	if err := ref.Get(ctx, &dataToMove); err != nil {
+		log.Fatal(err)
+	}
+	if err := ref.Delete(ctx); err != nil {
+		log.Fatal(err)
+	}
+	ref = client.NewRef(donePathDB + updateID)
+	if err := ref.Set(ctx, &dataToMove); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "Task %s, marked as done", updateID)
+}
+
 func getIDTask(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "getByID")
+	var data Task
+	params := mux.Vars(r)
+	taskID := params["id"]
+
+	ref := client.NewRef(todoPathDB + taskID)
+	if err := ref.Get(ctx, &data); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "Data of TaskID %s:\nTitle: %s\nDesc: %s\nDate: %s\n", taskID, data.Title, data.Desc, data.Date)
 }
